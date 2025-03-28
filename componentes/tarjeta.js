654910b00d7a51fa7be6e0db066516f88d6de6a8
icalvo0130 <isacalvo30@MacBook-Pro-de-Isa.local>
2025-03-28 18:26:59 -0500

    luchando con mi componente

--- componentes/tarjeta.js
+++ componentes/tarjeta.js
@@ -1,25 +1,36 @@
 class Cards extends HTMLElement {
     constructor() {
         super()
+
+        async function infromacion() {
+            const response = await fetch(request);
+            const infocompleta = await informacion.json();
+        }
     }
 
     connectedCallback(){
         const shadow = this.attachShadow({ mode: "open" });
         this.tarjeta = HTMLElement(div)
 
+
         this.tarjeta.innerHTML = /*html*/ `
-        
-        
-        
+
+        <style>
+        </style>
+
+        <div> 
+        <img src= https://forms.gle/vEZ2LBP3g7KAHLEQ6>
+        <h2>${'destino'}</h2>
+        <h3>${'costo'}</h3>
+        <p>${'descripcion'}</p>
+        <p>${'actividades'}</p>
+        <p>${'disponibilidad'}</p>
+        <p>${'alojamiento'}</p>
+        </div>
         `
     }
 
-
-    
-        'destino', 'duracion', 'costo', 'descripcion', 'actividades','imagen', 'disponibilidad', 'calificacion','alojamiento', 'guia'
-    
-
-    
-
 }
+
+export default Cards
 customElements.define("cards-eventos", Cards);
